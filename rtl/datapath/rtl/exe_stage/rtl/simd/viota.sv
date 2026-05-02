b24a79c23564f9f0e87db6ea2525fdb79ad1ef8d
vcastilla <119356332+vcastilla@users.noreply.github.com>
2026-05-02 13:50:01 +0200

    Reduce counter width in viota
    The maximum possible value of `count` fits inside VMAXELEM_LOG bits (instead of hardcoded 64 bits)

--- rtl/datapath/rtl/exe_stage/rtl/simd/viota.sv
+++ rtl/datapath/rtl/exe_stage/rtl/simd/viota.sv
@@ -30,11 +30,8 @@ module viota
     input logic                 use_mask_i,        //
     output bus_simd_t           data_vd_o       // bus_simd_t result
 );
-function [63:0] trunc_64bits(input [64:0] val_in);
-    trunc_64bits = val_in[63:0];
-endfunction
 
-logic [63:0] count;
+logic [VMAXELEM_LOG-1:0] count;
 bus_simd_t data_vd_i;
 
 always_comb begin
@@ -47,8 +44,8 @@ always_comb begin
                     if (use_mask_i & !data_vm_i[i]) begin
                         data_vd_i[(i*8)+:8] = data_old_vd[(i*8)+:8];
                     end else begin
-                        data_vd_i[(i*8)+:8] = count[7:0];
-                        count = trunc_64bits(count + data_vs2_i[i]);
+                        data_vd_i[(i*8)+:8] = 8'(count);
+                        count = count + data_vs2_i[i];
                     end                       
                 end                
             end
@@ -57,8 +54,8 @@ always_comb begin
                     if (use_mask_i & !data_vm_i[i]) begin
                         data_vd_i[(i*16)+:16] = data_old_vd[(i*16)+:16];
                     end else begin
-                        data_vd_i[(i*16)+:16] = count[15:0];
-                        count = trunc_64bits(count + data_vs2_i[i]);
+                        data_vd_i[(i*16)+:16] = 16'(count);
+                        count = count + data_vs2_i[i];
                     end      
                 end
             end
@@ -67,24 +64,20 @@ always_comb begin
                     if (use_mask_i & !data_vm_i[i]) begin
                         data_vd_i[(i*32)+:32] = data_old_vd[(i*32)+:32];
                     end else begin
-                        data_vd_i[(i*32)+:32] = count[31:0];
-                        count = trunc_64bits(count + data_vs2_i[i]);
+                        data_vd_i[(i*32)+:32] = 32'(count);
+                        count = count + data_vs2_i[i];
                     end       
                 end
             end
-
             SEW_64 : begin
                 for (int i = 0; i < (VLEN/64); i++) begin
                     if (use_mask_i & !data_vm_i[i]) begin
                         data_vd_i[(i*64)+:64] = data_old_vd[(i*64)+:64];
                     end else begin
-                        data_vd_i[(i*64)+:64] = count[63:0];
-                        count = trunc_64bits(count + data_vs2_i[i]);
+                        data_vd_i[(i*64)+:64] = 64'(count);
+                        count = count + data_vs2_i[i];
                     end  
                 end
-            end
-            default : begin
-                count = '0;
             end
         endcase
     end else begin
